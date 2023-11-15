a0e7d89af241b1cc27a10cea591e7d86a7139744
Aktimelke <andreynpl19@gmail.com>
2023-11-15 14:35:29 +0300

    modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,32 @@
+import Home from "./pages/home/Home.jsx";
+import TopBar from "./components/topbar/TopBar.jsx";
+import Single from "./pages/single/Single.jsx";
+import Write from "./pages/write/Write.jsx";
+import Settings from "./pages/settings/Settings.jsx";
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-function Home() {
-  return <div>Hello World</div>;
-}
-
+import { useContext } from "react";
+import { Context } from "./context/Context.js";
+import Footer from "./footer.jsx";
 
 function App() {
- 
+  const { user } = useContext(Context);
   return (
     <Router>
-      
+      <TopBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        </Routes>
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
+
 
 export default App;
